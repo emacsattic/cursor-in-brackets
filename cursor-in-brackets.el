5fe41e8f23971bcbfa081677dce200f973c3c515
yascentur <yascentur@gmail.com>
2013-05-26 15:28:24 +0900

    Version 1.0.0

--- cursor-in-brackets.el
+++ cursor-in-brackets.el
@@ -4,7 +4,7 @@
 
 ;; Author:   yascentur <screenname at gmail dot com>
 ;; Keywords: cursor bracket quote
-;; Version:  0.1.0
+;; Version:  1.0.0
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
